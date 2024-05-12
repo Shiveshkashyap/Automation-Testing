9d668655a72d4b64cf1209e6f382ee1f53825fbf
shiveshkashyap <shiv99esh@gmail.com>
2024-05-12 14:10:48 +0530

    2nd commit

--- src/revisionLecture.java
+++ src/revisionLecture.java
@@ -1,6 +1,6 @@
 public class revisionLecture {
     public static void main(String[]args){
         revisionLecture c1= new revisionLecture();
-        c1.equals()
+        c1.equals("Ram");
     }
 }
